ac57571344960135fb98ea876b010e1fc8d0272e
river_is_line <dmstjs7047@hanmail.net>
2022-03-08 21:44:52 +0900

    ⚡️ :: (#9) sign in publishing

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.gsps.gsp_android.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gsps.gsp_android.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+    }
+}
