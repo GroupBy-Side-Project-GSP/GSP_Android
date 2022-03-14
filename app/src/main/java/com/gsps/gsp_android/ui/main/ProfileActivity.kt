68cf10c4a6ffdcd872835f571b44e859c989f5f9
Choi YoonChan <choyc9239@naver.com>
2022-03-14 17:30:15 +0900

    ⚡ :: 프로필편집 주석 삭제 및 이미지 크기 처리 수정

--- app/src/main/java/com/gsps/gsp_android/ui/main/ProfileActivity.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/ProfileActivity.kt
@@ -7,43 +7,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Gallery
+import android.widget.ImageView
 import android.widget.Toast
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.ActivityProfileBinding
 import com.gsps.gsp_android.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class ProfileActivity : BaseActivity<ActivityProfileBinding>(
 R.layout.activity_profile
 ) {
 
     private lateinit var pbinding: ActivityProfileBinding
-
-    val PERM_STORAGE=9
-    val PERM_CAMEARA=10
-
-    val REQ_GALLERL = 12
-
+    val REQ_GALLERL = 1
 
     override fun initView() {
         pbinding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(pbinding.root)
 
-        binding.IbProfileImage.setOnClickListener{
+        pbinding.IbProfileImage.setOnClickListener{
             openGallary()
 
         }
-        binding.BtnProfileComplete.setOnClickListener{
+        pbinding.BtnProfileComplete.setOnClickListener{
             next()
         }
     }
+
+
+
     fun openGallary(){
         val intent=Intent(Intent.ACTION_PICK)
         intent.type=MediaStore.Images.Media.CONTENT_TYPE
         startActivityForResult(intent, REQ_GALLERL)
 
     }
+
+
     @Override
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -51,50 +52,23 @@ R.layout.activity_profile
             when(requestCode){
                 REQ_GALLERL -> {
                     data?.data.let { uri ->
+                        pbinding.IbProfileImage.setScaleType(ImageView.ScaleType.FIT_XY)
+                        pbinding.IbProfileImage.setBackgroundColor(getColor(R.color.white))
                         pbinding.IbProfileImage.setImageURI(uri)
                     }
                 }
             }
         }
     }
+
+
     fun next(){
-        if(pbinding.EdProfileName.text.toString()==null){
+        if(pbinding.EdProfileName.text.isEmpty()){
             Toast.makeText(this, "기업 이름이 비어있습니다.", Toast.LENGTH_SHORT).show()
         }else{
             val intent = Intent(this,InterestActivity::class.java)
+            startActivity(intent)
         }
 
     }
-
-
-
-
 }
-
-
-
-
-
-
-//        pbinding.IbProfileImage.setOnClickListener{
-//            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
-//            intent.setType("image/*")
-//            startActivityForResult(intent,1)
-//        }
-//
-//        @Override
-//        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//            super.onActivityResult(requestCode, resultCode, data)
-//            if( resultCode == Activity.RESULT_OK){
-//                if( requestCode == 1) {
-//                    var ImnageData: Uri? = data?.data
-//                    Toast.makeText(this,ImnageData.toString(), Toast.LENGTH_SHORT ).show()
-//                    try {
-//                        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, ImnageData)
-//                        GImageView.setImageBitmap(bitmap)
-//                    } catch (e:Exception) {
-//                        e.printStackTrace()
-//                    }
-//                }
-//            }
-//        }
