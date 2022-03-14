8810025e6465951e03150a3ec1681f3be86144a7
Choi YoonChan <choyc9239@naver.com>
2022-03-14 17:29:06 +0900

    ⚡ :: interestActivity 수정 및 업데이트(리사이클러뷰 ->checkbox 활용)

--- app/src/main/java/com/gsps/gsp_android/ui/main/InterestActivity.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/InterestActivity.kt
@@ -1,148 +1,103 @@
 package com.gsps.gsp_android.ui.main
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.*
+import androidx.activity.viewModels
+import androidx.annotation.CheckResult
+import androidx.annotation.RequiresApi
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.ActivityInterestBinding
-import com.gsps.gsp_android.ui.adapter.InterestAdapter
+import com.gsps.gsp_android.databinding.ActivityProfileBinding
 import com.gsps.gsp_android.ui.base.BaseActivity
 import com.gsps.gsp_android.ui.model.InterestModel
-import com.gsps.gsp_android.ui.viewmodel.InterestViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.collections.ArrayList
 
-//class InterestActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_interest)
-//    }
-//}
 
-@AndroidEntryPoint
+
+//@AndroidEntryPoint
 class InterestActivity : BaseActivity<ActivityInterestBinding>(
     R.layout.activity_interest
 ) {
     private lateinit var ibinding: ActivityInterestBinding
-    lateinit var interestViewModel: InterestViewModel
 
     companion object{
-        var count: Int =0
-    }
-    val interestList = arrayListOf(
-        InterestModel("디자인"),
-        InterestModel("경영&회계"),
-        InterestModel("보건&의료"),
-        InterestModel("환경&에너지"),
-        InterestModel("재료"),
-        InterestModel("전기&전자"),
-        InterestModel("건설"),
-        InterestModel("화학&바이오"),
-        InterestModel("기획&마케팅"),
-        InterestModel("인쇄&공예"),
-        InterestModel("영업판매"),
-        InterestModel("사회목지"),
-        InterestModel("사무"),
-        InterestModel("섬유&의복")
-    )
-
-    override fun initView(){
-        adpaterConnection(interestList)
-
-
-//        ibinding = ActivityInterestBinding.inflate(layoutInflater)
-//        setContentView(ibinding.root)
-//
-//        val interestList = arrayListOf(
-//            InterestModel("디자인"),
-//            InterestModel("경영&회계"),
-//            InterestModel("보건&의료"),
-//            InterestModel("환경&에너지"),
-//            InterestModel("재료"),
-//            InterestModel("전기&전자"),
-//            InterestModel("건설"),
-//            InterestModel("화학&바이오"),
-//            InterestModel("기획&마케팅"),
-//            InterestModel("인쇄&공예"),
-//            InterestModel("영업판매"),
-//            InterestModel("사회목지"),
-//            InterestModel("사무"),
-//            InterestModel("섬유&의복")
-//        )
-//
-//        ibinding.RvInterestMenu.layoutManager = StaggeredGridLayoutManager(5,StaggeredGridLayoutManager.HORIZONTAL)
-//        ibinding.RvInterestMenu.setHasFixedSize(true)
-
-
-
-//        ibinding.RvInterestMenu.adapter = InterestAdapter(interestList).setItemClickListener(object: InterestAdapter.OnItemClickListener{
-//            var count: Int=0
-//
-//            override fun onCheckedChanged(v: View, position: Int, isChecked: Boolean) {
-//                when(isChecked){
-//                    false -> count++
-//
-//                }
-//
-//
-//
-//
-//            }
-//        })
-
-        ibinding.RvInterestMenu.adapter = InterestAdapter(interestList).setItemClickListener(object : InterestAdapter.OnItemClickListener{
-            override fun onClick(v: View, position: Int) {
-
-                var checkedlist : ArrayList<Boolean> = arrayListOf(false)
-
-                checkedlist.set(position,true)
-
-            }
-        }
-
-//            override fun onClick(v: View, position: Int) {
-//                val clickcheck: Boolean =false
-//
-//                when(clickcheck){
-//                    false-> ibinding.RvInterestMenu[]
-//                }
-//
-//            }
-
-
+        var count:Int =0
     }
 
-    fun adpaterConnection(interestlist: ArrayList<InterestModel>){
+
+    override fun initView() {
         ibinding = ActivityInterestBinding.inflate(layoutInflater)
         setContentView(ibinding.root)
-
-
-
-        ibinding.RvInterestMenu.layoutManager = StaggeredGridLayoutManager(5,StaggeredGridLayoutManager.HORIZONTAL)
-        ibinding.RvInterestMenu.setHasFixedSize(true)
+        check()
     }
 
-    fun observEvent(interestlist: ArrayList<InterestModel>){
-
-        interestViewModel = ViewModelProvider(this).get(InterestViewModel::class.java)
-
-        interestViewModel.countText.observe(this, Observer{
-            ibinding.TvInterestCount.text =it
-        })
-
-        interestViewModel.init()
 
 
-        ibinding.RvInterestMenu.adapter = InterestAdapter(interestList).setItemClickListener(object : InterestAdapter.OnItemClickListener{
-            override fun onCheckedChanged(v: View, position: Int, isChecked: Boolean) {
-                //해당 아이템 뷰의 값을
-                v.setOnClickListener{
 
+    fun check(){
+        var listener =CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                when(buttonView){
+                    ibinding.designCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.managementCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.healthCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.environmentCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.materialCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.electronicCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.erectionCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.chemistryCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.planCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.printCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.salesCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.welfareCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.AffairsCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    ibinding.clothCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                }
+            }else{
+                when(buttonView){
+                    ibinding.designCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.managementCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.healthCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.environmentCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.materialCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.electronicCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.erectionCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.chemistryCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.planCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.printCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.salesCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.welfareCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.AffairsCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
+                    ibinding.clothCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
                 }
             }
         }
+        ibinding.designCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.managementCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.healthCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.environmentCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.materialCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.electronicCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.erectionCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.chemistryCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.planCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.printCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.salesCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.welfareCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.AffairsCheckBox.setOnCheckedChangeListener(listener)
+        ibinding.clothCheckBox.setOnCheckedChangeListener(listener)
     }
 }
+
