768042b81cba266d3a1bef99fc5a50a0f2c867b6
Choi YoonChan <choyc9239@naver.com>
2022-03-15 17:23:46 +0900

    ⚡ :: interestActivity 개수 제한 완료

--- app/src/main/java/com/gsps/gsp_android/ui/main/InterestActivity.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/InterestActivity.kt
@@ -19,7 +19,7 @@ import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.ActivityInterestBinding
 import com.gsps.gsp_android.databinding.ActivityProfileBinding
 import com.gsps.gsp_android.ui.base.BaseActivity
-import com.gsps.gsp_android.ui.model.InterestModel
+//import com.gsps.gsp_android.ui.model.InterestModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 import kotlin.collections.ArrayList
@@ -44,27 +44,46 @@ class InterestActivity : BaseActivity<ActivityInterestBinding>(
     }
 
 
-
-
     fun check(){
         var listener =CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked){
-                when(buttonView){
-                    ibinding.designCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.managementCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.healthCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.environmentCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.materialCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.electronicCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.erectionCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.chemistryCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.planCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.printCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.salesCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.welfareCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.AffairsCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
-                    ibinding.clothCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                if(count<=4){
+                    when(buttonView){
+                        ibinding.designCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.managementCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.healthCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.environmentCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.materialCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.electronicCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.erectionCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.chemistryCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.planCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.printCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.salesCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.welfareCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.AffairsCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                        ibinding.clothCheckBox -> {ibinding.TvInterestCount.text="(${++count}/5)"}
+                    }
+                }else{
+                    Toast.makeText(this, "선택 갯수 초과입니다.", Toast.LENGTH_SHORT).show()
+                    when(buttonView){
+                        ibinding.designCheckBox -> {ibinding.designCheckBox.isChecked=false}
+                        ibinding.managementCheckBox -> {ibinding.managementCheckBox.isChecked=false}
+                        ibinding.healthCheckBox -> {ibinding.healthCheckBox.isChecked=false}
+                        ibinding.environmentCheckBox -> {ibinding.environmentCheckBox.isChecked=false}
+                        ibinding.materialCheckBox -> {ibinding.materialCheckBox.isChecked=false}
+                        ibinding.electronicCheckBox -> {ibinding.electronicCheckBox.isChecked=false}
+                        ibinding.erectionCheckBox -> {ibinding.erectionCheckBox.isChecked=false}
+                        ibinding.chemistryCheckBox -> {ibinding.chemistryCheckBox.isChecked=false}
+                        ibinding.planCheckBox -> {ibinding.planCheckBox.isChecked=false}
+                        ibinding.printCheckBox -> {ibinding.printCheckBox.isChecked=false}
+                        ibinding.salesCheckBox -> {ibinding.salesCheckBox.isChecked=false}
+                        ibinding.welfareCheckBox -> {ibinding.welfareCheckBox.isChecked=false}
+                        ibinding.AffairsCheckBox -> {ibinding.AffairsCheckBox.isChecked=false}
+                        ibinding.clothCheckBox -> {ibinding.clothCheckBox.isChecked=false}
+                    }
                 }
+
             }else{
                 when(buttonView){
                     ibinding.designCheckBox -> {ibinding.TvInterestCount.text="(${--count}/5)"}
