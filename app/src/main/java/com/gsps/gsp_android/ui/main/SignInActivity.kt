bb3e6a44e80d0c1ec566d8e566058d4239194e1e
river_is_line <dmstjs7047@hanmail.net>
2022-03-18 21:13:52 +0900

    ♻️:: 로그인 binding 재설정

--- app/src/main/java/com/gsps/gsp_android/ui/main/SignInActivity.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SignInActivity.kt
@@ -1,13 +1,8 @@
 package com.gsps.gsp_android.ui.main
 
 import android.graphics.drawable.GradientDrawable
-import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.ActivitySignInBinding
 import com.gsps.gsp_android.ui.base.BaseActivity
@@ -15,27 +10,32 @@ import java.util.regex.Pattern
 
 class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
 
-    private val userIdTil: TextInputLayout by lazy { findViewById(R.id.sign_in_til_id) }
-    private val userId: TextInputEditText by lazy { findViewById(R.id.sign_in_et_id) }
-    private val pwTil: TextInputLayout by lazy { findViewById(R.id.sign_in_til_pw) }
-    private val pw: TextInputEditText by lazy { findViewById(R.id.sign_in_et_pw) }
-    private val buttonSignIn: AppCompatButton by lazy { findViewById(R.id.sign_in_btn_sign_in) }
     var booleanId: Boolean = false
     var booleanPw: Boolean = false
     lateinit var backgroundColorButtonSignIn: GradientDrawable
 
-    override fun ActivitySignInBinding.onCreate(){
-        
+
+    fun checkId() {
+        val id = binding.signInEtId.text.toString().trim()
+        val patternId: String = """^[0-9a-zA-Z!@#$%^+_\-=]*$"""
+        val match = Pattern.matches(patternId, id)
+
+        return if (!match) {
+            binding.signInEtId.setTextColor(getColor(R.color.system_red))
+            binding.signInTilId.error = getString(R.string.error_wrong_id)
+            booleanId = false
+        } else {
+            binding.signInEtId.setTextColor(getColor(R.color.black))
+            binding.signInTilId.error = null
+            booleanId = id != ""
+        }
     }
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+    override fun initView() {
+        backgroundColorButtonSignIn = binding.signInBtnSignIn.background as GradientDrawable
 
-        backgroundColorButtonSignIn = buttonSignIn.background as GradientDrawable
-
-        userId.addTextChangedListener(object : TextWatcher {
+        binding.signInEtId.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -47,11 +47,11 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sig
             override fun afterTextChanged(s: Editable?) {}
         })
 
-        pw.addTextChangedListener(object : TextWatcher {
+        binding.signInEtPw.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                booleanPw = pw.text.toString().trim() != ""
+                booleanPw = binding.signInEtPw.text.toString().trim() != ""
                 if (booleanId && booleanPw) backgroundColorButtonSignIn.setColor(getColor(R.color.main))
                 else backgroundColorButtonSignIn.setColor(getColor(R.color.main_lighten))
             }
@@ -59,26 +59,4 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sig
             override fun afterTextChanged(s: Editable?) {}
         })
     }
-
-
-    fun checkId() {
-        val id = userId.text.toString().trim()
-        val patternId: String = """^[0-9a-zA-Z!@#$%^+_\-=]*$"""
-        val match = Pattern.matches(patternId, id)
-
-        return if (!match) {
-            userId.setTextColor(getColor(R.color.system_red))
-            userIdTil.error = getString(R.string.error_wrong_id)
-            booleanId = false
-        } else {
-            userId.setTextColor(getColor(R.color.black))
-            userIdTil.error = null
-            booleanId = id != ""
-        }
-    }
-
-    override fun initView() {
-        TODO("Not yet implemented")
-    }
-
 }
