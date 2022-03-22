d9ec42bce80817dbd0a623a280f7c0eee18d6ec0
chc <choyc9239@naver.com>
2022-03-22 23:26:21 +0900

    ♻️ :: mypage 리펙토리

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/MyPageActivity.kt
@@ -0,0 +1,15 @@
+package com.gsps.gsp_android.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.ActivityMyPageBinding
+import com.gsps.gsp_android.ui.base.BaseActivity
+
+class MyPageActivity : BaseActivity<ActivityMyPageBinding>(
+    R.layout.activity_my_page
+) {
+    override fun initView() {
+
+    }
+}
