97bbfd371855c56f4cee99f3316dcc2c2aaf1c68
chc <choyc9239@naver.com>
2022-03-23 05:23:18 +0900

    ♻️ :: 마이페이지의 모양 수정

--- app/src/main/java/com/gsps/gsp_android/ui/main/MyPageActivity.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/MyPageActivity.kt
@@ -1,5 +1,6 @@
 package com.gsps.gsp_android.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gsps.gsp_android.R
@@ -9,7 +10,30 @@ import com.gsps.gsp_android.ui.base.BaseActivity
 class MyPageActivity : BaseActivity<ActivityMyPageBinding>(
     R.layout.activity_my_page
 ) {
+
+
     override fun initView() {
 
+        binding.ConMyProEdite.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.ConMyOffice.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ConMyInterest.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ConMyNotion.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ConMyAppSetting.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
