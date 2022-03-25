0ed5bb3b38306f36066a6c1228060d610a653225
chc <choyc9239@naver.com>
2022-03-26 02:18:39 +0900

    ⚡️ :: 마이페이지 공지사항 xml

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SubAnnouncementActivity.kt
@@ -0,0 +1,16 @@
+package com.gsps.gsp_android.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.ActivityAnnouncementBinding
+import com.gsps.gsp_android.ui.base.BaseActivity
+
+class SubAnnouncementActivity : BaseActivity<ActivityAnnouncementBinding>(
+    R.layout.activity_sub_announcement
+) {
+
+    override fun initView() {
+
+    }
+}
