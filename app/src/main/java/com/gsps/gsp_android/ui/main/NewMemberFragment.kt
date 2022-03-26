4abc464e556d93b39a43ca8de7796615e4d07567
river_is_line <dmstjs7047@hanmail.net>
2022-03-27 03:07:42 +0900

    ♻️:: NewMemberFragment binding

--- app/src/main/java/com/gsps/gsp_android/ui/main/NewMemberFragment.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/NewMemberFragment.kt
@@ -1,11 +1,13 @@
 package com.gsps.gsp_android.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.FragmentNewMemberBinding
+import com.gsps.gsp_android.ui.base.BaseFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [NewMemberFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NewMemberFragment : Fragment() {
+class NewMemberFragment : BaseFragment<FragmentNewMemberBinding>(R.layout.fragment_new_member) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -56,6 +58,10 @@ class NewMemberFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun initView() {
+
     }
 
 }
