4e6686e69af2786534bcc77aef9ea3e6c4df6e22
chc <choyc9239@naver.com>
2022-03-27 18:44:17 +0900

    ⚡️:: 검색 xml 추가

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SearchFragment.kt
@@ -0,0 +1,20 @@
+package com.gsps.gsp_android.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.FragmentSearchBinding
+import com.gsps.gsp_android.ui.base.BaseFragment
+
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(
+    R.layout.fragment_search
+){
+    override fun initView() {
+        
+    }
+
+}
