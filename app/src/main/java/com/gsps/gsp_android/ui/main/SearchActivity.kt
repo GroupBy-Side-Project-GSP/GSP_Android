254bdb251606893095840f70478314f1443ec707
chc <choyc9239@naver.com>
2022-03-27 18:09:56 +0900

    ⚡️:: search xml 추가

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SearchActivity.kt
@@ -0,0 +1,18 @@
+package com.gsps.gsp_android.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.ActivitySearchBinding
+import com.gsps.gsp_android.ui.base.BaseActivity
+
+class SearchActivity : BaseActivity<ActivitySearchBinding>(
+    R.layout.activity_search
+) {
+
+
+    override fun initView() {
+
+
+    }
+}
