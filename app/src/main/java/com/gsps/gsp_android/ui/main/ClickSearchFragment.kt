145ead99bdab960fcff710c2ba9100ded661caa8
chc <choyc9239@naver.com>
2022-03-27 23:11:02 +0900

    ⚡️:: 검색 기능 추가

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/ClickSearchFragment.kt
@@ -0,0 +1,19 @@
+package com.gsps.gsp_android.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.FragmentSearchBinding
+import com.gsps.gsp_android.ui.base.BaseFragment
+
+
+class ClickSearchFragment : BaseFragment<>(
+    R.layout.fragment_click_search
+) {
+    override fun initView() {
+
+    }
+}
