011c56a9699f7ccd7135e3f8b3229ebea6fe6dc9
chc <choyc9239@naver.com>
2022-03-29 01:41:45 +0900

    ⚡️ :: 최근기록,검색어 activity 추가

--- app/src/main/java/com/gsps/gsp_android/ui/main/ClickSearchFragment.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/ClickSearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.FragmentClickSearchBinding
 import com.gsps.gsp_android.databinding.FragmentSearchBinding
@@ -14,7 +15,34 @@ import com.gsps.gsp_android.ui.base.BaseFragment
 class ClickSearchFragment : BaseFragment<FragmentClickSearchBinding>(
     R.layout.fragment_click_search
 ) {
-    override fun initView() {
 
+    val adapter by lazy { SearchTextAdapter() }
+    val recentrecordadapter by lazy { SearchRecentRecordAdapter() }
+
+    override fun initView() {
+        adapter()
+        recentrecordadapter()
+    }
+
+    fun adapter(){
+        val testList = listOf<SearchTextModel>(
+            SearchTextModel("검색어"),
+        )
+
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        adapter.addItems(testList)
+        binding.rvClickSearchText.layoutManager=linearLayoutManager
+        binding.rvClickSearchText.adapter=adapter
+    }
+
+    fun recentrecordadapter(){
+        val testList2 = listOf<SearchRecentRecordModel>(
+            SearchRecentRecordModel("최근기록")
+        )
+
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        recentrecordadapter.addItems(testList2)
+        binding.rvRecentRecord.layoutManager=linearLayoutManager
+        binding.rvRecentRecord.adapter=recentrecordadapter
     }
 }
