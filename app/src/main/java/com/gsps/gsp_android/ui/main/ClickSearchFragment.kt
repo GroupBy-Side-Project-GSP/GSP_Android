bc5e494f72d38bbfcf5e3e1467615e7ea3738544
chc <choyc9239@naver.com>
2022-03-28 15:07:45 +0900

    ⚡️:: 검색 텍스트 검색 xml

--- app/src/main/java/com/gsps/gsp_android/ui/main/ClickSearchFragment.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/ClickSearchFragment.kt
@@ -6,11 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.FragmentClickSearchBinding
 import com.gsps.gsp_android.databinding.FragmentSearchBinding
 import com.gsps.gsp_android.ui.base.BaseFragment
 
 
-class ClickSearchFragment : BaseFragment<>(
+class ClickSearchFragment : BaseFragment<FragmentClickSearchBinding>(
     R.layout.fragment_click_search
 ) {
     override fun initView() {
