2b6341877b93731cb7242e3a75a546d58ae470b8
river_is_line <dmstjs7047@hanmail.net>
2022-03-29 08:51:16 +0900

    ⚡️:: 기본 Activity, xml 추가

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/MemberInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.gsps.gsp_android.ui.main
+
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.ActivityMemberInformationBinding
+import com.gsps.gsp_android.ui.base.BaseActivity
+
+class MemberInformationActivity :
+    BaseActivity<ActivityMemberInformationBinding>(R.layout.activity_member_information) {
+    override fun initView() {
+
+    }
+}
