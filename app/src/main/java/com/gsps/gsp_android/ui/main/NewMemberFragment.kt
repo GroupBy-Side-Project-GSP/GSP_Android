bf28f765cc74ffdeeb3da9b135a615ef61120706
river_is_line <dmstjs7047@hanmail.net>
2022-03-28 21:57:16 +0900

    ⚡️:: 신규가입회사 모델 추가

--- app/src/main/java/com/gsps/gsp_android/ui/main/NewMemberFragment.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/NewMemberFragment.kt
@@ -1,13 +1,12 @@
 package com.gsps.gsp_android.ui.main
 
-import android.view.LayoutInflater
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.FragmentNewMemberBinding
 import com.gsps.gsp_android.ui.base.BaseFragment
 
 class NewMemberFragment : BaseFragment<FragmentNewMemberBinding>(R.layout.fragment_new_member) {
     override fun initView() {
-        val inflater = LayoutInflater.from(this)
+        
     }
 
 }
---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/NewMemberModel.kt
@@ -0,0 +1,9 @@
+package com.gsps.gsp_android.ui.main
+
+data class NewMemberModel(
+    var imageMain: String,
+    var imageLogo: String,
+    var name: String,
+    var explain: String,
+    var interest: Boolean
+)
