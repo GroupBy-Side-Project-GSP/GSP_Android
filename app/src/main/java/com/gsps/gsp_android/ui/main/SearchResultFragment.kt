3c2c9a46a9b29348ef24934b6df18a00984e7c14
chc <choyc9239@naver.com>
2022-03-29 01:42:05 +0900

    ⚡️ :: 검색결과 activity 추가

--- app/src/main/java/com/gsps/gsp_android/ui/main/SearchResultFragment.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SearchResultFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.FragmentSearchResultBinding
 import com.gsps.gsp_android.ui.base.BaseFragment
@@ -13,7 +14,21 @@ import com.gsps.gsp_android.ui.base.BaseFragment
 class SearchResultFragment : BaseFragment<FragmentSearchResultBinding>(
     R.layout.fragment_search_result
 ) {
-    override fun initView() {
+    val adapter by lazy { SearchResultAdapter() }
 
+    override fun initView() {
+        adapter()
+
+    }
+
+    fun adapter(){
+        val testList = listOf<SearchResultModel>(
+            SearchResultModel("회사내용","회사이름")
+        )
+
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        adapter.addItems(testList)
+        binding.rvSearchResult.layoutManager=linearLayoutManager
+        binding.rvSearchResult.adapter=adapter
     }
 }
