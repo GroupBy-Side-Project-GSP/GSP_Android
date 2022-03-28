d933c3dea262c59769941a46612867fc005688b4
chc <choyc9239@naver.com>
2022-03-29 01:40:51 +0900

    ⚡️:: 텍스트검색,체크박스 검색 activity 추가

--- app/src/main/java/com/gsps/gsp_android/ui/main/SearchFragment.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/SearchFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gsps.gsp_android.R
 import com.gsps.gsp_android.databinding.FragmentSearchBinding
 import com.gsps.gsp_android.ui.base.BaseFragment
@@ -13,8 +17,92 @@ import com.gsps.gsp_android.ui.base.BaseFragment
 class SearchFragment : BaseFragment<FragmentSearchBinding>(
     R.layout.fragment_search
 ){
-    override fun initView() {
-        
+
+    companion object{
+        var count:Int =0
     }
 
+    override fun initView() {
+        check()
+    }
+
+    fun check(){
+        val listener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                when(buttonView){
+                    binding.searchDesignCheckBox -> {}
+                    binding.searchManagementCheckBox -> {}
+                    binding.searchHealthCheckBox -> {}
+                    binding.searchEnvironmentCheckBox -> {}
+                    binding.searchMaterialCheckBox -> {}
+                    binding.searchElectronicCheckBox -> {}
+                    binding.searchErectionCheckBox -> {}
+                    binding.searchChemistryCheckBox -> {}
+                    binding.searchPlanCheckBox -> {}
+                    binding.searchPrintCheckBox -> {}
+                    binding.searchSalesCheckBox -> {}
+                    binding.searchWelfareCheckBox -> {}
+                    binding.searchAffairsCheckBox -> {}
+                    binding.searchClothCheckBox -> {}
+                }
+                if(count==0){
+                    binding.btnSelectSearch.isEnabled=false
+                }else{
+                    binding.btnSelectSearch.isEnabled=true
+                }
+                when(buttonView){
+                    binding.searchDesignCheckBox -> {binding.searchDesignCheckBox.isChecked=false}
+                    binding.searchManagementCheckBox -> {binding.searchManagementCheckBox.isChecked=false}
+                    binding.searchHealthCheckBox -> {binding.searchHealthCheckBox.isChecked=false}
+                    binding.searchEnvironmentCheckBox -> {binding.searchEnvironmentCheckBox.isChecked=false}
+                    binding.searchMaterialCheckBox -> {binding.searchMaterialCheckBox.isChecked=false}
+                    binding.searchElectronicCheckBox -> {binding.searchElectronicCheckBox.isChecked=false}
+                    binding.searchErectionCheckBox -> {binding.searchErectionCheckBox.isChecked=false}
+                    binding.searchChemistryCheckBox -> {binding.searchChemistryCheckBox.isChecked=false}
+                    binding.searchPlanCheckBox -> {binding.searchPlanCheckBox.isChecked=false}
+                    binding.searchPrintCheckBox -> {binding.searchPrintCheckBox.isChecked=false}
+                    binding.searchSalesCheckBox -> {binding.searchSalesCheckBox.isChecked=false}
+                    binding.searchWelfareCheckBox -> {binding.searchWelfareCheckBox.isChecked=false}
+                    binding.searchAffairsCheckBox -> {binding.searchAffairsCheckBox.isChecked=false}
+                    binding.searchClothCheckBox -> {binding.searchClothCheckBox.isChecked=false}
+                }
+            }else{
+                when(buttonView){
+                    binding.searchDesignCheckBox -> {}
+                    binding.searchManagementCheckBox -> {}
+                    binding.searchHealthCheckBox -> {}
+                    binding.searchEnvironmentCheckBox -> {}
+                    binding.searchMaterialCheckBox -> {}
+                    binding.searchElectronicCheckBox -> {}
+                    binding.searchErectionCheckBox -> {}
+                    binding.searchChemistryCheckBox -> {}
+                    binding.searchPlanCheckBox -> {}
+                    binding.searchPrintCheckBox -> {}
+                    binding.searchSalesCheckBox -> {}
+                    binding.searchWelfareCheckBox -> {}
+                    binding.searchAffairsCheckBox -> {}
+                    binding.searchClothCheckBox -> {}
+                }
+                if(count==0){
+                    binding.btnSelectSearch.isEnabled=false
+                }else{
+                    binding.btnSelectSearch.isEnabled=true
+                }
+            }
+        }
+        binding.searchDesignCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchManagementCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchHealthCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchEnvironmentCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchMaterialCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchElectronicCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchErectionCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchChemistryCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchPlanCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchPrintCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchSalesCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchWelfareCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchAffairsCheckBox.setOnCheckedChangeListener(listener)
+        binding.searchClothCheckBox.setOnCheckedChangeListener(listener)
+    }
 }
