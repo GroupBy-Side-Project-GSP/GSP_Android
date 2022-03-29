79d61a24d953236e3149a88323797f99859fb8f5
river_is_line <dmstjs7047@hanmail.net>
2022-03-29 11:57:52 +0900

    ⚡️:: 회사 상세 페이지 Activity 추가

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/MemberInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.gsps.gsp_android.ui.main
+
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.ActivityMemberInfoBinding
+import com.gsps.gsp_android.ui.base.BaseActivity
+
+class MemberInfoActivity :
+    BaseActivity<ActivityMemberInfoBinding>(R.layout.activity_member_info) {
+    override fun initView() {
+
+    }
+}
