23e466b43222a392a86b7d788fce819fbe9a582f
river_is_line <dmstjs7047@hanmail.net>
2022-03-29 20:37:13 +0900

    ⚡️:: 달력 외부 틀 생성

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/CalendarFragment.kt
@@ -0,0 +1,11 @@
+package com.gsps.gsp_android.ui.main
+
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.FragmentCalendarBinding
+import com.gsps.gsp_android.ui.base.BaseFragment
+
+class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
+    override fun initView() {
+
+    }
+}
