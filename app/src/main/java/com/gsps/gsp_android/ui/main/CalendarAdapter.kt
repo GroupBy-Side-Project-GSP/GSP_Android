118e6603220d0abe19710183f9fca328021f6e65
river_is_line <dmstjs7047@hanmail.net>
2022-04-08 23:00:12 +0900

    ⚡️:: 달력 Adapter 추가

--- app/src/main/java/com/gsps/gsp_android/ui/main/CalendarAdapter.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/CalendarAdapter.kt
@@ -1,15 +1,36 @@
 package com.gsps.gsp_android.ui.main
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.gsps.gsp_android.databinding.ItemDayBinding
+import com.gsps.gsp_android.databinding.ItemDayEmptyBinding
 
-class CalendarAdapter(context: Context, private val itemList: MutableList<CalendarDayModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class CalendarAdapter(context: Context, private val itemList: MutableList<CalendarDayModel>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        if (viewType == 0) {
+            val itemDayBinding =
+                ItemDayBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return ViewHolder(itemDayBinding)
+        }
+        else if (viewType == 1) {
+            val itemDayEmptyBinding = ItemDayEmptyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return EmptyViewHolder(itemDayEmptyBinding)
+        }
+
+    }
+
+    class EmptyViewHolder(itemDayEmptyBinding: ItemDayEmptyBinding) : RecyclerView.ViewHolder() {
+
+    }
+
+    class ViewHolder(itemDayBinding: ItemDayBinding) : RecyclerView.ViewHolder() {
+
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -20,4 +41,11 @@ class CalendarAdapter(context: Context, private val itemList: MutableList<Calend
         return itemList.size
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            itemList[position].isSelected -> 2
+            itemList[position].plan.isEmpty() -> 0
+            else -> 1
+        }
+    }
 }
--- app/src/main/java/com/gsps/gsp_android/ui/main/CalendarDayModel.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/CalendarDayModel.kt
@@ -3,6 +3,7 @@ package com.gsps.gsp_android.ui.main
 import java.time.LocalDate
 
 data class CalendarDayModel(
-    val date: LocalDate,
-    val plan: MutableList<PlanModel>
+    var date: LocalDate,
+    var plan: MutableList<PlanModel>,
+    var isSelected: Boolean
 )
