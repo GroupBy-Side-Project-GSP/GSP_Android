a5819b691599423d6680054a410c40c6f68e9884
river_is_line <dmstjs7047@hanmail.net>
2022-04-13 00:46:24 +0900

    ⚡️:: 초기 작성 화면 xml

---
+++ app/src/main/java/com/gsps/gsp_android/ui/main/MemberInfoWritingActivity.kt
@@ -0,0 +1,12 @@
+package com.gsps.gsp_android.ui.main
+
+import com.gsps.gsp_android.R
+import com.gsps.gsp_android.databinding.ActivityMemberInfoWritingBinding
+import com.gsps.gsp_android.ui.base.BaseActivity
+
+class MemberInfoWritingActivity :
+    BaseActivity<ActivityMemberInfoWritingBinding>(R.layout.activity_member_info_writing) {
+    override fun initView() {
+
+    }
+}
