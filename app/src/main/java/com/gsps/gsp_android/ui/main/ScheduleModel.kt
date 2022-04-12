0d65a6bb59a8056e444c9ffa2a13993c6aa9f0bc
river_is_line <dmstjs7047@hanmail.net>
2022-04-12 21:46:28 +0900

    ⚡️:: schedule dialog xml 추가

--- app/src/main/java/com/gsps/gsp_android/ui/main/ScheduleModel.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/ScheduleModel.kt
@@ -1,14 +1,15 @@
 package com.gsps.gsp_android.ui.main
 
-import java.time.LocalDate
 import java.time.LocalTime
 
 data class ScheduleModel(
-    var date: LocalDate = LocalDate.now(),
+    var year: Int = 2022,
+    var month: Int = 4,
+    var date: Int = 12,
     var startTime: LocalTime = LocalTime.now(),
     var endTime: LocalTime = LocalTime.now(),
     var info: String = "",
     var place: String = "",
-    var proposalCompanyId: Int = 0,
-    var participantCompanyId: Int = 0
+    var companyName: String = "",
+    var logoUrl: String = ""
 )
