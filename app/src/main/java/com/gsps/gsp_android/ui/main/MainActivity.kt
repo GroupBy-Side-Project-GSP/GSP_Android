f91390656717a01cc57523fade90b4ed025b70e2
river_is_line <dmstjs7047@hanmail.net>
2022-04-19 19:50:43 +0900

    ♻️:: init mainActivity

--- app/src/main/java/com/gsps/gsp_android/ui/main/MainActivity.kt
+++ app/src/main/java/com/gsps/gsp_android/ui/main/MainActivity.kt
@@ -2,18 +2,11 @@ package com.gsps.gsp_android.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
 import com.gsps.gsp_android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragment = CalendarFragment()
-        val fragmentManager: FragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.calendarContainer, fragment)
-        fragmentTransaction.commit()
     }
 }
